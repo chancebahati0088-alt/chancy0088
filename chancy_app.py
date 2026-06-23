f620dee569758ed10226f0d2cf0d69f6eb80647d
chancebahati0088-alt <chancebahati0088@gmail.com>
2026-06-23 15:48:22 +0200

    Update chancy_app.py

--- chancy_app.py
+++ chancy_app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Bonjour je suis Chancy, une intelligence artificielle. Dites-moi comment je peux vous aider"
-if __name__=="__main__"
+if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
