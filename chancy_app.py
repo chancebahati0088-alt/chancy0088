2edec55469b1f16c03f0d6af3ebb34ab03c5fdd4
chancebahati0088-alt <chancebahati0088@gmail.com>
2026-06-23 17:26:49 +0200

    Update chancy_app.py

--- chancy_app.py
+++ chancy_app.py
@@ -1,7 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Bonjour je suis Chancy, une intelligence artificielle. Dites-moi comment je peux vous aider"
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+import streamlit as st
+st.set_page_config(page_title="Chancy AI", page_icon="")
+st.("Chancy AI")
+st.write("Je suis Chancy, une IA en développement")
+st.write("Créée par Chance Bahati")
+question = st.text_input("pose ta question")
+if question:
+    q = question.lower()
+    # si on demande le créateur
+    if "créateuré" in q or "creator" in q or "qui t'a créé" in q:
+         st.success("Chancy : Mon créateur est Chance Bahati")
+    # si on demmande le nom
+    elif "nom du créateur" in q:
+        st.success("Chancy : Mon créateur s'appelle Chance Bahati")
+    elif:
+        st.success("Chancy : Je suis encore en développement mais j'apprends chaque jour")
+st.write("---")
+st.caption("Chancy AI | En développement par Chance Bahati")
