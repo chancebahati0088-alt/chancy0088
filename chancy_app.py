4507929fc10e89a5b009628c5a901694c0de17d9
chancebahati0088-alt <chancebahati0088@gmail.com>
2026-06-30 16:54:43 +0200

    chancy_app.py

--- chancy_app.py
+++ chancy_app.py
@@ -108,8 +108,9 @@ try:
             reponse = "Chancy : Je suis encore en développement — je ne comprends pas parfaitement, mais j'apprends !"
 
         st.session_state.messages.append(reponse)
+        st.session_state.input = ""
         st.rerun()
-    st.caption("Chancy AI | Projet de Chance Bahati")
+        st.caption("Chancy AI | Projet de Chance Bahati")
 
 except Exception as e:
     # Friendly message shown to users
