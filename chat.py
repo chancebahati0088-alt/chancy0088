f4a590428539bca946c7ea2744127499e5429e4d
chancebahati0088-alt <chancebahati0088@gmail.com>
2026-07-09 12:46:19 +0200

    chat,py

--- chat.py
+++ chat.py
@@ -1,1 +1,24 @@
+st.divider()
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+if prompt := st.chat_input("Écris ton message…"):
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
+
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    reponse = "Bonjour ! Je suis Chancy AI, votre assistante personnelle en programmation."
+    st.session_state.messages.append(
+        {"role": "assistant", "content": reponse}
+    )
+
+    with st.chat_message("assistant"):
+        st.write(reponse)
